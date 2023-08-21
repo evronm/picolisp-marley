4e3db752680addd2e26e1a84fcb3ab925cab0ff5
evronm <evronm@gmail.com>
2023-08-21 09:17:04 -0700

    none of this works.  trying to retucture the prototpye feature.  Done a lot of playing, need to start using git as I am pretty sure I am on the right track.

--- misc/testapp.sql
+++ misc/testapp.sql
@@ -1,5 +1,5 @@
 
-create table users (id integer primary key, last_name text, first_name text, email text unique, phone text, address text, zip text, type text);
+create table users (id integer primary key, pw password, last_name text, first_name text, email text unique, phone text, address text, zip text, type text);
 create table services (id integer primary key, vendor_id integer, name text, description text, duration integer, price decimal);
 create table itineraries (id integer primary key, user_id, provider_id, start datetime);
 create table itineraries_services (id integer primary key, itinerary_id integer, service_id, start time);
