25fe837a370799e1547866f325e17caca1dc4596
evronm <evronm@gmail.com>
2023-09-09 06:44:40 -0700

    enforce returning only proto's from correct namespace

--- misc/testapp.sql
+++ misc/testapp.sql
@@ -1,5 +1,5 @@
 
-create table users (id integer primary key, pw password, last_name text, first_name text, email text unique, phone text, address text, zip text, type text);
-create table services (id integer primary key, vendor_id integer, name text, description text, duration integer, price currency, currency text);
-create table itineraries (id integer primary key, user_id, provider_id, start datetime);
+create table users (id integer primary key, name text, pw password, email text unique, phone text, address text, zip text, type text);
+create table services (id integer primary key, vendor_id integer, name text, description text, duration integer, capacity integer, max_group_size integer, price currency, currency text);
+create table itineraries (id integer primary key, user_id, ppl integer, provider_id, start datetime);
 create table itineraries_services (id integer primary key, itinerary_id integer, service_id, start time);
--- misc/testdata.sql
+++ misc/testdata.sql
@@ -4,9 +4,9 @@ delete from services;
 delete from itineraries;
 delete from itineraries_services;
 
-insert into users values (null, "last", "first_name", "email", "phone", "address", "zip", null);
-insert into users values (null, "last", "first_name", "email1", "phone", "address", "zip", "v");
-insert into users values (null, "last", "first_name", "email2", "phone", "address", "zip", "p");
-insert into services values (null, 2, "service1 name", "service 1 description ", 90, 200);
-insert into itineraries values (null, 1, 3, "20240101 - 10:00AM");
+insert into users values (null, "user", "pw", "email", "phone", "address", "zip", null);
+insert into users values (null, "vendor", "pw", "emailv", "phonev", "address", "zip", "v");
+insert into users values (null, "provider", "pw", "emailp", "phonep", "address", "zip", "p");
+insert into services values (null, 2, "service1 name", "service 1 description ", 90, 10, 6, 20000, "usd");
+insert into itineraries values (null, 1, 4, 3, "20240101 - 10:00AM");
 insert into itineraries_services values (null, 1, 1, "10:30AM");
